957e306eccc50e9f83ce1e0c73a3fe928d01ead2
Thomas Jakobi <buero@treehillstudio.de>
2025-11-17 12:43:07 +0100

    Added translation using Weblate (Spanish)

---
+++ core/components/smartrouting/lexicon/es/default.inc.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Default Lexicon Entries for SmartRouting
+ *
+ * @package smartrouting
+ * @subpackage lexicon
+ */
+$_lang['smartrouting'] = '';
+$_lang['smartrouting.refresh_cache'] = '';
