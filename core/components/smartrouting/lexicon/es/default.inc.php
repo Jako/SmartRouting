6d453510a1137eee7b0b653f3d9bea49ac95cdfb
Thomas Jakobi <buero@treehillstudio.de>
2025-11-17 12:48:46 +0100

    Deleted translation using Weblate (Spanish)

--- core/components/smartrouting/lexicon/es/default.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Default Lexicon Entries for SmartRouting
- *
- * @package smartrouting
- * @subpackage lexicon
- */
-$_lang['smartrouting'] = '';
-$_lang['smartrouting.refresh_cache'] = '';
